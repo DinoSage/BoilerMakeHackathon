feb089542c793837b9ec78a6f40240865af3148e
Aniket-Rai1 <ridge.arai@gmail.com>
2023-01-21 13:38:59 -0500

    Base Classes

---
+++ core/src/com/mygdx/app/User.java
@@ -0,0 +1,79 @@
+package com.mygdx.app;
+
+import java.util.ArrayList;
+
+public class User {
+    private final long start;
+    private int points;
+    private int percentTaskCompleted;
+
+    private ArrayList<Task> tasks;
+
+    private int streakCounter;
+
+    public User(int hours, int percentTaskCompleted, int points, ArrayList<Task> tasks, int streakCounter) {
+        this.tasks = tasks;
+        this.percentTaskCompleted = percentTaskCompleted;
+        this.points = points;
+        this.streakCounter = streakCounter;
+        start = System.currentTimeMillis();
+
+    }
+
+    public int getStreakCounter() {
+        return streakCounter;
+    }
+
+    public void setStreakCounter(int streakCounter) {
+        this.streakCounter = streakCounter;
+    }
+
+    public int getPercentTaskCompleted() {
+        return percentTaskCompleted;
+    }
+
+    public void setPercentTaskCompleted(int percentTaskCompleted) {
+        this.percentTaskCompleted = percentTaskCompleted;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public double elapsedTimeHours() {
+        long now = System.currentTimeMillis();
+        return (now - start) / (60.0 * 60.0 * 1000.0);
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public ArrayList<Task> updateTasks(Task task) {
+        tasks.add(task);
+        return tasks;
+    }
+
+    public int getNumberOfTasks(ArrayList<Task> tasks) {
+        return tasks.size();
+    }
+
+    public int getNumberOfTasksCompleted(ArrayList<Task> tasks) {
+        int counter = 0;
+
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).isTaskComplete()) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
