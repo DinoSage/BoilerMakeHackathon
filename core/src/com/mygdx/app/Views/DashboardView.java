c69bb260d2e613a7e4af6b26ee2b3f694a24f4d7
DinoSage <dinosage5632@gmail.com>
2023-01-22 03:56:15 -0500

    Minor Dashboard View Progress

--- core/src/com/mygdx/app/Views/DashboardView.java
+++ core/src/com/mygdx/app/Views/DashboardView.java
@@ -1,10 +1,13 @@
 package com.mygdx.app.Views;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.mygdx.app.AppConstants;
 import com.mygdx.app.AssetStorage;
+import com.mygdx.app.Leaderboard;
 
 public class DashboardView extends Table {
 
@@ -14,6 +17,12 @@ public class DashboardView extends Table {
         final Skin skin = AssetStorage.getInstance().skin;
 
         this.setDebug(AppConstants.DEBUG);
+        Leaderboard leaderboard = assets.currentUser.getLeaderboard();
+        ProgressBar progress = new ProgressBar(0, leaderboard.getHoursPerStreak(), leaderboard.getHoursPerStreak() * .001f, false, skin);
+
+        TextButton startBtn = new TextButton("Start", skin, "small");
+        TextButton endBtn = new TextButton("End", skin, "small");
     }
+
 
 }
